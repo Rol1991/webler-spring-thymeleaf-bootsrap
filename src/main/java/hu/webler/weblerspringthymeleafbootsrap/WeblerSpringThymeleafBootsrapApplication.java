47089a1801f0a99c1ab5168b21ee663187d42d77
Szőke Roland <szokeroland0@gmail.com>
2024-01-29 13:15:20 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerspringthymeleafbootsrap/WeblerSpringThymeleafBootsrapApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerspringthymeleafbootsrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerSpringThymeleafBootsrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerSpringThymeleafBootsrapApplication.class, args);
+	}
+
+}
